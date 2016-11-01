5a738bfa582786df5e1caa9552a13962a386a6fd
unleet <unleet.42@gmail.com>
2016-10-31 20:22:33 -0400

    Bump Meteor version

--- base/scripts/lib/install_meteor.sh
+++ base/scripts/lib/install_meteor.sh
@@ -1,3 +1,3 @@
 set -e
 
-curl -sL https://install.meteor.com/?release=1.3.4.4 | sed s/--progress-bar/-sL/g | /bin/sh
+curl -sL https://install.meteor.com/?release=1.3.5.1 | sed s/--progress-bar/-sL/g | /bin/sh
